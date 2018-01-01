88f43f38cae3a987fbc280ffce3c1d61876e9fc2
shekharbhardwaj <shekhar.nik@gmail.com>
2017-12-31 23:35:42 -0500

    refactoring code, initial page is ready

--- js/app.js
+++ js/app.js
@@ -2,24 +2,41 @@ const $studentList = $('.student-list').children();
 const $pageDivSelector = $('.page');
 const paginationDiv = '<div class="pagination"><ul class="hrefstudent"></ul></div>';
 const $paginationList = $('.pagination ul');
-
+const totalStudentShow =  10;
 //Appending pagination to Page div
-$pageDivSelector.append($(paginationDiv));
-//console.log($studentList.length);
+$pageDivSelector.append(paginationDiv);
+//console.log($studentList);
 let internalIndex = 0;
-let temp  = 0;
+let studentObjList = [];
+//let tempStudentList = [];
 
-for(let index = 0; index <=  $studentList.length; index += 1){
-	if (internalIndex != 10) {
-			internalIndex += 1;
-			}
-	else if (internalIndex == 10) {
-						$('.hrefstudent').append('<li><a href="#">'+(temp += 1)+'</a></li>');
-						internalIndex = 0;
-						//console.log('Button '+ (temp += 1));
-	} else if (($studentList - index) < 10) {
-						 $$('.hrefstudent').append('<li><a href="#">'+(temp += 1)+'</a></li>');
-						 //console.log('Button '+ (temp += 1));
-						 }
-	
+$('.initial-list').toggle(false);
+
+while($studentList.length) {
+    studentObjList.push($studentList.splice(0,10));
 }
+
+console.log(studentObjList.length);
+
+for (let i = 0; i < studentObjList.length; i += 1){
+			const hrefId = i +1;
+			if(hrefId != 1){
+				$('<ul class="student-list" id="page'+hrefId+'"></ul>').insertAfter('.page-header');
+				for(let j = 0; j < studentObjList[i].length; j +=1){
+					$('#page'+hrefId).append(studentObjList[i][j]);
+					$('#page'+hrefId).toggle(false);
+			}
+			} else {
+				$('<ul class="student-list" id="page'+hrefId+'"></ul>').insertAfter('.page-header');
+				for(let j = 0; j < studentObjList[i].length; j +=1){
+						$('#page'+hrefId).append(studentObjList[i][j]);
+			}
+			}
+			if (hrefId != 1) {
+			$('.hrefstudent').append('<li><a id="hrefId'+hrefId+'" href="#page'+hrefId+'">'+hrefId+'</a></li>');
+			} else {
+				$('.hrefstudent').append('<li><a id="hrefId'+hrefId+'" href="#page'+hrefId+'">'+hrefId+'</a></li>');
+				$('#hrefId'+hrefId).addClass('active');
+			}
+}
+	
